fc983c56fc65ef11016b21f13ecdda810dccb20f
mohamed-saud <mo7amed.saud2020@gmail.com>
2024-02-06 20:59:59 +0200

    new steps container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Steps />
     </div>
   );
 }
 
+function Steps() {
+  const [step, setstep] = useState(1);
+  const [visbile, setvisbile] = useState(true);
+  const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+  ];
+
+  return (
+    <div className="container-steps">
+      <button className="isOpen" onClick={() => setvisbile(!visbile)}>
+        x
+      </button>
+      <div className={!visbile ? "visbile steps" : "steps"}>
+        <div className="numbers">
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
+        </div>
+
+        <p className="message">{messages[step - 1]}</p>
+
+        <div className="buttons">
+          <button
+            onClick={() => (step > 1 ? setstep(step - 1) : null)}
+            className="previous"
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => (step < 3 ? setstep(step + 1) : null)}
+            className="next"
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default App;
